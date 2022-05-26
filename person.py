9bd49df5d69785ea2481eac57a4a896fd0817de5
Kal'ko Ivan <vano2149@gmail.com>
2022-05-26 14:52:08 +0300

    Page 72!

--- person.py
+++ person.py
@@ -2,14 +2,47 @@
 """
 
 class Person:
+    """
+    """
     def __init__(self, name, job=None, pay=0):
         self.name = name
         self.job = job
         self.pay = pay
+    
+    def lastName(self):
+        return self.name.split()[-1]
+    
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
+    def __repr__(self):
+        return f"<Person:{self.name}, {self.pay}>"
+
+
+class Manager(Person):
+    """
+    """
+    def giveRaise(self, percent, bonus=.10):
+        Person.giveRaise(self, percent + bonus)
 
 
 if __name__ == '__main__':
-    bob = Person('Bob Smit')
+    bob = Person('Bob Smith')
     sue = Person('Sue Jones', job = 'dev', pay = 100000)
     print(bob.name, bob.pay)
     print(sue.name, sue.pay)
+    print(bob.lastName())
+    print('-' * 26)
+    print(sue.lastName())
+    sue.giveRaise(.10)
+    print(sue)
+    print('-' * 26)
+    tom = Manager('Tom Jones', 'mgr', 50000)
+    tom.giveRaise(.10)
+    print(tom.lastName())
+    print(tom)
+    print('--All three--')
+    for obj in (bob, sue, tom):
+        obj.giveRaise(.10)
+        print(obj)
+
