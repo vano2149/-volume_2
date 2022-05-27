f654185b3b874022532d97c1b57433433b61f964
Kal'ko Ivan <vano2149@gmail.com>
2022-05-27 15:56:10 +0300

    Create new file classtools.py!

---
+++ classtools.py
@@ -0,0 +1,35 @@
+"""
+Смешанные утелиты и инструменты для классов!
+"""
+
+class AttrDisplay:
+    """
+    Предоставляет наследуемый 
+    метод перегрузки отображения.
+    """
+    def gatherAttrs(self):
+        attrs = []
+        for key in sorted(self.__dict__):
+            attrs.append(f"{key}={getattr(self, key)}")
+        return ', '.join(attrs)
+    
+    def __repr__(self):
+
+        return f'{self.__class__.__name__} {self.gatherAttrs()}'
+
+if __name__ == '__main__':
+    class TopTest(AttrDisplay):
+        count = 0
+
+        def __init__(self):
+            self.attr1 = TopTest.count
+            self.attr2 = TopTest.count + 1
+            TopTest.count += 2
+
+    class SubTest(TopTest):
+        pass
+
+X,Y = TopTest(), SubTest()
+
+print(X)
+print(Y)
