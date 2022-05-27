07fa64982c3ad18d147aaf89d5d1c2acb43df4d7
Kal'ko Ivan <vano2149@gmail.com>
2022-05-27 12:05:35 +0300

    Modified a person.py file!

--- person.py
+++ person.py
@@ -23,29 +23,44 @@ class Manager(Person):
     """
     """
     def __init__(self,name, pay):
-        Person.__init__(self,name,'mgr',pay)
+        self.person = Person(name, 'mgr', pay)
     
     def giveRaise(self, percent, bonus=.10):
-        Person.giveRaise(self, percent + bonus)
+        self.person.giveRaise(percent + bonus)
+
+    def __getattr__(self, attr):
+        return getattr(self.person, attr)
+
+    def __repr__(self):
+        return str(self.person)
+
+class Departament:
+    def __init__(self, *args):
+        
+        self.members = list(args)
+    
+    def addMember(self,person):
+        self.members.append(person)
+    
+    def giveRaise(self, percent):
+        for person in self.members:
+            person.giveRaise(percent)
+    
+    def showAll(self):
+        for person in self.members:
+            print(person)
 
 
 if __name__ == '__main__':
+    print('-' * 26)
     bob = Person('Bob Smith')
-    sue = Person('Sue Jones', job = 'dev', pay = 100000)
-    print(bob.name, bob.pay)
-    print(sue.name, sue.pay)
-    print(bob.lastName())
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    tom = Manager('Tom Smith', 50000)
+    
+
+    development = Departament(bob, sue)
+    development.addMember(tom)
+    development.giveRaise(.10)
+    development.showAll()
     print('-' * 26)
-    print(sue.lastName())
-    sue.giveRaise(.10)
-    print(sue)
-    print('-' * 26)
-    tom = Manager('Tom Jones', 50000)
-    tom.giveRaise(.10)
-    print(tom.lastName())
-    print(tom)
-    print('--All three--')
-    for obj in (bob, sue, tom):
-        obj.giveRaise(.10)
-        print(obj)
 
