50f2c1b1995f3334204d4631c8cdeb6e097f04f8
Kal'ko Ivan <vano2149@gmail.com>
2022-05-31 16:17:30 +0300

    Page 133!

--- squares.py
+++ squares.py
@@ -2,3 +2,16 @@
 Squares.py file!
 """
 
+class Squares:
+    def __init__(self, start, stop):
+        self.value = start - 1
+        self.stop = stop
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.value == self.stop:
+            raise StopIteration
+        self.value += 1
+        return self.value ** 2
