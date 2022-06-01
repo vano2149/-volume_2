40ecd84a5a208134b86ea0a748f1c7a56e2c5ddf
Kal'ko Ivan <vano2149@gmail.com>
2022-06-01 15:51:48 +0300

    Page 143!!

--- squares_nonyield.py
+++ squares_nonyield.py
@@ -1,5 +1,25 @@
 """
+Page 143!
 """
 
 class Squares:
-    pass
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+
+    def __iter__(self):
+        """"""
+        return SquaresIter(self.start, self.stop)
+        
+class SquaresIter:
+    ''''''
+    def __init__(self, start, stop):
+        ''''''
+        self.value = start - 1
+        self.stop = stop
+    def __next__(self):
+        ''''''
+        if self.value == self.stop:
+            raise StopIteration
+        self.value += 1
+        return self.value ** 2
