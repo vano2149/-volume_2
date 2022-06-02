d0a80739f34344ecff974bb209cf9338ccb5dfa3
Kal'ko Ivan <vano2149@gmail.com>
2022-06-02 14:54:46 +0300

    Create a new class!

--- commuter.py
+++ commuter.py
@@ -14,3 +14,36 @@ class Commuter1:
 
         print('radd', self.val, other)
         return other + self.val
+
+class Commuter2:
+
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+
+    def __radd__(self, other):
+        return  self._add__(other)
+
+class Commuter3:
+
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+
+    def __radd__(self, other):
+        return self + other
+
+class Commuter4:
+
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+    __radd__ =__add__
