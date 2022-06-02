561faf0c975a28d674ec6d358f9090f9fb9a80d1
Kal'ko Ivan <vano2149@gmail.com>
2022-06-02 11:40:03 +0300

    Modified a private.py file!!

--- private0.py
+++ private0.py
@@ -1,3 +1,33 @@
 """
 """
 
+class PrivateExc(Exception):
+    pass
+
+class Privacy:
+    def __setattr__(self, attrname, value):
+
+        if attrname in self.privates:
+            raise PrivateExc(attrname, self)
+
+        else:
+            self.__dict__[attrname] = value
+
+
+class Test1(Privacy):
+    privates = ['age']
+
+class Test2(Privacy):
+    privates = ['name', 'pay']
+    def __init__(self):
+        self.__dict__['name'] = 'Tom'
+
+if __name__ == '__main__':
+    x = Test1()
+    y = Test2()
+
+    x.name = 'Bob'
+    print(x.name)
+
+    y.age = 30
+    print(y.age)
