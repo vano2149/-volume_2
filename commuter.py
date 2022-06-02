f9f19ddb7d30cf3caa6f6570ea408ec0ed3af532
Kal'ko Ivan <vano2149@gmail.com>
2022-06-02 14:40:20 +0300

    Create a new file!!!

--- commuter.py
+++ commuter.py
@@ -1,3 +1,16 @@
 """
 """
 
+class Commuter1:
+
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+
+    def __radd__(self, other):
+
+        print('radd', self.val, other)
+        return other + self.val
