af5caf59a6b21e89dc44824d9c03227fc06c2f55
Kal'ko Ivan <vano2149@gmail.com>
2022-06-03 16:35:58 +0300

    Create new class a processor!

--- pizzashop.py
+++ pizzashop.py
@@ -8,25 +8,39 @@ class Customer:
     """
     """
     def __init__(self, name):
+        """
+        """
         self.name = name
+
     def order(self, server):
+        """
+        """
         print(f"{self.name} orders form {server}")
+
     def pay(self, server):
+        """
+        """
         print(f"{self.name} pays for item to {server}")
 
 class Oven:
     def bake(self):
+        """
+        """
         print("oven bakes")
 
 class PizzaShop:
     """
     """
     def __init__(self):
+        """
+        """
         self.server = Server("Pat")
         self.chef = PizzaRobot("Bob")
         self.oven = Oven()
-    
+
     def order(self, name):
+        """
+        """
         customer = Customer(name)
         customer.order(self.server)
         self.chef.work()
@@ -34,7 +48,11 @@ class PizzaShop:
         customer.pay(self.server)
 
 if __name__ == '__main__':
+
+    print('-' * 26)
     scene = PizzaShop()
     scene.order('Homer')
+    print('-' * 26)
     print('...')
     scene.order('Shaggy')
+    print('-' * 26)
