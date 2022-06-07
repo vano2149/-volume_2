be6ee713a345836e821d30b19df60d125e5960c1
Kal'ko Ivan <vano2149@gmail.com>
2022-06-07 15:33:01 +0300

    Create new an factory.py file!

---
+++ factory.py
@@ -0,0 +1,18 @@
+"""
+"""
+
+def factory(aClass, *args, **kwargs):
+    return aClass(*args, **kwargs)
+
+class Spam:
+    def doit(self, message):
+        print(message)
+
+class Person:
+    def __init__(self, name, job=None):
+        self.name = name
+        self.job = job
+
+object1= factory(Spam)
+object2 = factory(Person, "Arthur", 'King')
+object3 = factory(Person, name='Brian')
