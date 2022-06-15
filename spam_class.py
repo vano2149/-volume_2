931ca4b66347b3348d92de968260dc8a0491c0e2
Kal'ko Ivan <vano2149@gmail.com>
2022-06-15 14:57:22 +0300

    Create classmethod in spam_class.py file!

--- spam_class.py
+++ spam_class.py
@@ -1,7 +1,7 @@
 """
 spam_class.py file!!
 """
-
+'''
 class Spam:
     numInstances  = 0
 
@@ -10,6 +10,17 @@ class Spam:
 
     def printNumInstances():
         print(f"Number of instances: {Spam.numInstances}")
-    
+
     printNumInstances = staticmethod(printNumInstances)
+'''
+
+class Spam:
+    numInstances = 0
+    def __init__(self):
+        Spam.numInstances += 1
+    
+    def printNumInstances(cls):
+        print(f"Number of instances: {cls.numInstances}")
+    
+    printNumInstances = classmethod(printNumInstances)
 
