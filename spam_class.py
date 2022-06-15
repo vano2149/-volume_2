4ca4ba735b23164521aab07554121772846d5f61
Kal'ko Ivan <vano2149@gmail.com>
2022-06-15 14:22:49 +0300

    Modified spam_class.py file!

--- spam_class.py
+++ spam_class.py
@@ -2,17 +2,14 @@
 spam_class.py file!!
 """
 
-
-
 class Spam:
-    numInstances = 0
+    numInstances  = 0
 
     def __init__(self):
+        Spam.numInstances += 1
 
-        Spam.numInstances =Spam.numInstances + 1
-
-    def printNumInstances(self):
-
-        print(f"Number of instances created: {Spam.numInstances}")
-
+    def printNumInstances():
+        print(f"Number of instances: {Spam.numInstances}")
+    
+    printNumInstances = staticmethod(printNumInstances)
 
