229070ae7e1f68c67348b39eafc66eb13c4fac37
Kal'ko Ivan <vano2149@gmail.com>
2022-06-15 15:29:01 +0300

    Modified spam_class.py file!

--- spam_class.py
+++ spam_class.py
@@ -20,7 +20,15 @@ class Spam:
         Spam.numInstances += 1
     
     def printNumInstances(cls):
-        print(f"Number of instances: {cls.numInstances}")
+        print(f"Number of instances: {cls.numInstances, cls}")
     
     printNumInstances = classmethod(printNumInstances)
 
+class Sub(Spam):
+    def printNumInstances(cls):
+        print("Extra stuff...", cls)
+        Spam.printNumInstances()
+    printNumInstances = classmethod(printNumInstances)
+
+class Other(Spam):
+    pass
