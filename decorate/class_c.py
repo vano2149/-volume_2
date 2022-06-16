8b1f3f4f20baf527eb1f6fa05b7d1d52b06a5e57
Kal'ko Ivan <vano2149@gmail.com>
2022-06-16 15:00:15 +0300

    Modified class_c.py file!

--- decorate/class_c.py
+++ decorate/class_c.py
@@ -3,8 +3,48 @@ class_c.py file!
 Cтраница 286! Положительные стороны super!
 изменения деревьев и координирование!
 """
+"""
+class A:
+    def __init__(self):
+        print('A.__init__')
 
-class C:
+class B(A):
+    def __init__(self):
+        print('B.__init__')
+        A.__init__(self)
+
+class C(A):
+    def __init__(self):
+        print('C.__init__')
+        A.__init__(self)
+
+class D(B, A):
+    def __init__(self):
+        B.__init__(self)
+        C.__init__(self)
+"""
+
+class A:
+    def __init__(self): print('A.__init__')
+
+class B(A):
+    def __init__(self): print("B.__init__"); super().__init__()
+
+class C(A):
+    def __init__(self): print('C.__init__'); super().__init__()
+
+class D(B, C):
     pass
 
 
+if __name__ == '__main__':
+    x = B()
+    print(f"Обращение к B: {x}")
+    print('-'* 25)
+    c = C()
+    print(f"Обращение к С: {c}")
+    print('-'* 25)
+    x = D()
+    print(f'Обращение к D: {x}')
+    print(B.__mro__)
+    print(D.__mro__)
