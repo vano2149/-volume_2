d00b8205a390e3e50b6fda337969939d86254577
Kal'ko Ivan <vano2149@gmail.com>
2022-06-23 15:28:22 +0300

    Create an oops.py file and Page -> 401!

---
+++ exception/oops.py
@@ -0,0 +1,24 @@
+"""
+file oops.py
+Page -> 401!
+Unicode and bytes list!
+"""
+
+class Oops(Exception):
+    """"""
+    def MyError():
+        raise Exception("MyError")
+
+def oops():
+    """"""
+    raise IndexError
+
+def oops1():
+    return oops()
+try:
+    oops1()
+except:
+    print('Гавно Индекс!')
+
+if __name__  == '__main__':
+    oops1()
