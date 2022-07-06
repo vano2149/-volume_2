2a64e41e9d8ce87e9373321309f8ea75a3f2ed5c
Kal'ko Ivan <vano2149@gmail.com>
2022-07-06 12:35:02 +0300

    Modified the decorator.py file!

--- decorate/decorator.py
+++ decorate/decorator.py
@@ -2,6 +2,7 @@
 decorator.py file!
 """
 
+"""
 class Tracer:
     def __init__(self, func):
         self.calls = 0
@@ -17,3 +18,27 @@ def spam(a, b, c):
     print(a + b + c)
 
 spam(1, 2, 3)
+"""
+class Tracer:
+    def __init__(self, func):
+        self.calls = 0
+        self.func = func
+    
+    def __call__(self, *args, **kwargs):
+        self.calls += 1
+        print(f'Call {self.calls} to {self.func.__name__}')
+        return self.func(*args, **kwargs)
+
+@Tracer
+def spam(a, b, c):
+    print(a + b + c)
+
+@Tracer
+def eggs(x, y):
+    print(x ** y)
+
+if __name__ == '__main__':
+    spam(1, 2, 3)
+    spam(a=4, b=5, c=6)
+    eggs(2, 16)
+    eggs(4, y=4)
