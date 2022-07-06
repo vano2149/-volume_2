f24867e3e8c363b3ef0c07f26becb84b12c81243
Kal'ko Ivan <vano2149@gmail.com>
2022-07-06 10:38:25 +0300

    Modified addr.py file

--- decorate/adder.py
+++ decorate/adder.py
@@ -2,3 +2,21 @@
 addr.py file!
 Page -> 505!
 """
+class decorator:
+    def __init__(self, func):
+        self.func = func
+    
+    def __call__(self, *args):
+        self.func(*args)
+    
+@decorator
+def func(x, y):
+    return print(x, y)
+
+
+class C:
+    @decorator
+    def method(x, y):
+        ...
+
+
