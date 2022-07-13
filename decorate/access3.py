f288d09e148f618ccb22eea188c6d9975f4c1790
Kal'ko Ivan <vano2149@gmail.com>
2022-07-13 11:20:29 +0300

    Modified an access3.py file!

--- decorate/access3.py
+++ decorate/access3.py
@@ -2,20 +2,46 @@
 access3.py file!
 """
 
-def accessControl(foilIf):
+from decorate.access2 import trace
+
+traceMe = False
+def trace(*args):
+    if traceMe:
+        print('[' + ' '.join(map(str, args))+ ']')
+
+def accessControl(failIf):
     def onDecorator(aClass):
         class onInstance:
             def __init__(self, *args, **kwargs):
                 self.__wrapped = aClass(*args, **kwargs)
+
             def __str__(self):
                 return str(self.__wrapped)
+
             def __add__(self, other):
                 return self.__wrapped + other
+
             def __getitem__(self, index):
                 return self.__wrapped[index]
+
             def __call__(self, *args, **kwargs):
                 return self.__wrapped(*args, **kwargs)
 
+            def __getattr__(self, attr):
+                trace('Get: ', attr)
+                if failIf(attr):
+                    raise TypeError(f'Private attribute fetch: {attr}')
+                else:
+                    return getattr(self.__wrapped, attr)
+                
+            def __setattr__(self, attr, value):
+                trace('Set: ', attr, value)
+                if attr =='_onInstance__wrapped':
+                    self.__dict__[attr] = value
+                elif failIf:
+                    raise TypeError(f'Private attribute change: {attr}')
+                else:
+                    setattr(self.__wrapped, attr, value)
         return onInstance
     return onDecorator
 
