cd29cced89f7ac05f44fd706935d7bcf05d2a7d6
Kal'ko Ivan <vano2149@gmail.com>
2022-07-14 12:09:01 +0300

    Cretae new an access5.py file!

--- decorate/access4.py
+++ decorate/access4.py
@@ -91,15 +91,15 @@ def accessControl(failIf):
 class BuiltinsMixin:
     def reroute(self, attr, *args, **kwargs):
         return self.__class__.__getattr__(self, attr)(*args, **kwargs)
-    
+
     def __add__(self, other):
         return self.reroute('__add__',other)
-    
+
     def __str__(self):
         return self.reroute('__str__')
-    
+
     def __getitem__(self, index):
         return self.reroute('__getitem__', index)
-    
+
     def __call__(self, *args, **kwargs):
         return self.reroute('__call__', *args, **kwargs)
---
+++ decorate/access5.py
@@ -0,0 +1,46 @@
+"""
+access5.py file!
+"""
+traceMe = False
+def trace(*args):
+    if traceMe:
+        print('[' + ' '.join(map(str, args))+ ']')
+
+
+def accessControl(failIf):
+    def onDecorator(aClass):
+        def getattributes(self, attr):
+            trace('Get:' + attr)
+            if failIf(attr):
+                raise TypeError(f'Privale attribute fetch: {attr}')
+            else:
+                return object.__getattribute__(self, attr)
+        def setattributes(self, attr, value):
+            trace('Set:' + attr)
+            if failIf(attr):
+                raise TypeError(f'Private attribute change {attr}')
+            else:
+                return object.__setattribute__(self, attr, value)
+        aClass.__getattribute__ = getattributes
+        aClass.__setattribute__ = setattributes
+        return aClass
+    return onDecorator
+
+def Private(*attributes):
+    return accessControl(failIf=(lambda attr: attr in attributes))
+
+def Public(*attributes):
+    return accessControl(failIf=(lambda attr: attr not in attributes))
+
+if __name__ == '__main__':
+    @Private('age')
+    class Person:
+        """
+        Doc String a Person class!
+        """
+        def __init__(self, name, age):
+            self.name = name
+            self.age = age
+
+    X = Person('bob smith', 40)
+    print(f"Hi my name is {X.name.title()}!")
