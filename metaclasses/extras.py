073d03f12d496d57a1af75feb0b6941ea5c0f52a
Kal'ko Ivan <vano2149@gmail.com>
2022-08-02 14:28:22 +0300

    Create new classes!

--- metaclasses/extras.py
+++ metaclasses/extras.py
@@ -2,13 +2,36 @@
 extras.py file!
 """
 
-class Extras:
-    def extra(self, args):
-        ...
+def extra(self, arg): ...
 
-class Client1(Extras): ...
-class Client2(Extras): ...
-class Client3(Extras): ...
+class Extras(type):
+    def __init__(Class, classname, superclasses, attributedict):
+        if required():
+            Class.extra = extra
+
+class Client1(metaclass=Extras): ...
+class Client2(metaclass=Extras): ...
+class Client3(metaclass=Extras): ...
+
+X = Client1()
+X.extra()
+
+
+def extra(self, arg): ...
+
+def extras(Class):
+    if required():
+        Class.extra = extra
+    return Class
+
+class Client1: ...
+Client1 = extras(Client1)
+
+class Client2: ...
+Client2 = extras(Client2)
+
+class Client3: ...
+Client3 = extras(Client3)
 
 X = Client1()
 X.extra()
